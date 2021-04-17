3f980b97275763206352e7266352a30eeed98aa0
Melih Şahin <melihsahin24@gmail.com>
2021-04-17 23:31:14 +0300

    complete create new article page desing

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/new', (req, res) => {
+  res.render('articles/new')
+});
+
+router.post('/', (req, res) => {
+  
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,20 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res ) => {
-  res.render('index')
+  const day  = new Date();
+  const articles = [
+    {
+      title: 'test makale',
+      createdAt: day.toDateString(),
+      description: 'test description',
+    },
+    {
+      title: 'test makale - 2',
+      createdAt: day.toDateString(),
+      description: 'test description  2',
+    },
+  ]
+  res.render('index', { articles: articles })
 });
 
 app.listen(3000);
