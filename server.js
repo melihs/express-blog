17c96a471873981b185be806d0c89726ea326580
Melih Şahin <melihsahin24@gmail.com>
2021-04-17 22:26:43 +0300

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/articles', articleRouter);
+
+app.get('/', (req, res ) => {
+  res.render('index')
+});
+
+app.listen(3000);
